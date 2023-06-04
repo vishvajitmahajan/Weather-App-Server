59885a3de0e1f2997bed9f8e1bfd31b87020a181
vishvajitmahajan <vishumahajan1301@gmail.com>
2023-06-04 18:08:29 +0530

    Update weatherSchema.js

--- models/weatherSchema.js
+++ models/weatherSchema.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const WeatherSchema = new mongoose.Schema({
   data: Object,
-});
+},{
+  timestamps:true);
 
 module.exports = mongoose.model("WeatherData", WeatherSchema);
