fcefce4d98b72204791ee5fa3acf1a298877e071
vishvajitmahajan <vishumahajan1301@gmail.com>
2023-06-04 17:10:16 +0530

    Update getWeatherData.js

--- controllers/getWeatherData.js
+++ controllers/getWeatherData.js
@@ -2,12 +2,17 @@ const WeatherSchema = require("../models/weatherSchema");
 
 const getWeatherData = async (req, res) => {
   try {
+    let result = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&appid=" +
+        process.env.API_KEY
+    );
+    let defaultData = await result.json();
     let weatherData = await WeatherSchema.find({});
     if (!weatherData)
       res.status(404).send({
         message: "data not found!",
       });
-    res.status(200).send(weatherData);
+    res.status(200).send({current:defaultData,history:weatherData});
   } catch (err) {
     res.status(400).send(err);
   }
